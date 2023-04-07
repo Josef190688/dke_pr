230e1a3f4f0849144873fa701c2b6591183c1b0a
Josef190688 <josef_niedereder@gmx.at>
2023-04-07 20:17:23 +0200

    flask install shell

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Das Ausführen dieser Shell legt ein Virtual Environment für das Projekt an
+# und installiert alle nötigen Flask Abhängigkeiten. 
+
+python3 -m venv venv
+chmod +x venv/bin/activate
+source venv/bin/activate
+pip install --upgrade pip
+pip install flask
+pip install python-dotenv
+pip install pipreqs
+pipreqs .
+pip install -r requirements.txt
+echo "Flask App installiert. Neues Terminal starten und 'flask run' eingeben."
